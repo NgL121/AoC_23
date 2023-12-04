4d5095159d0dad74a691ec6323c9837c26852425
Luong <n.luong@campus.lmu.de>
2023-12-04 16:07:16 +0100

    part 2

--- day04/sol.py
+++ day04/sol.py
@@ -1,20 +1,23 @@
 with open('input.txt', 'r') as data:
     data = [line.strip() for line in data.readlines()]
     output = 0
-    count_copies = [0] * len(data)
-    print(count_copies)
+    count_copies = [0] * (len(data) +1)
 
     # iterate over every line
     for line in data:
+
         # divide it into 2 parts
-        line = line.split(':')[1]
+        line = line.split(':')
+        card_id = int(line[0].split()[-1])
+        line = line[1]
+
+        count_copies[card_id] += 1
+
         line = line.split('|')
         # get the winning card
         winning_numbers = [int(num) for num in line[0].split()]
-        # print(winning_numbers)
 
         numbers = [int(num) for num in line[1].split()]
-        # print(numbers)
 
         exp = 0
         curr_point = 0
@@ -24,6 +27,11 @@ with open('input.txt', 'r') as data:
                 curr_point = 2**exp
                 exp += 1
 
+        if curr_point > 0:
+            for idx in range(card_id + 1, card_id + exp + 1):
+                count_copies[idx] += 1*count_copies[card_id]
+
         output += curr_point
 
     print(output)
+    print(sum(count_copies))
