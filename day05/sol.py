ced6d21b7a38704c2ad1d593054d7184a521283b
Luong <n.luong@campus.lmu.de>
2023-12-05 07:11:49 +0100

    Wanted to use search tree but I just can't sorry dsa teacher ಥ_ಥ

---
+++ day05/sol.py
@@ -0,0 +1,27 @@
+with open ('input.txt', 'r') as data:
+    lines = data.read().split('\n\n')
+
+    mapping = {}
+
+    seeds = lines[0].split(':')
+    seeds = [int(seed) for seed in seeds[1].split()]
+
+    pointer = seeds
+
+    for line in lines[1:]:
+        line = line.split('\n')
+        check_list = []
+        for find in pointer:
+            found = False
+            for maps in line[1:]:
+                maps = [int(num) for num in maps.split()]
+                if find in range(maps[1], maps[1] + maps[2]):
+                    check_list.append(maps[0] + find - maps[1])
+                    found = True
+
+            if not found:
+                check_list.append(find)
+
+        pointer = check_list
+
+    print(min(pointer))
