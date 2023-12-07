92d6ff1d1db6eaad725f27a2034c7d48e3ed6ec6
Luong <n.luong@campus.lmu.de>
2023-12-07 08:50:32 +0100

    finally...

--- day07/p2.py
+++ day07/p2.py
@@ -1,36 +1,50 @@
 CARDS = {'high': [], '1 pair': [], '2 pair': [], '3 kind': [], 'full': [], '4 kind': [], '5 kind': []}
-RANK = {}
 BID = {}
 CARD_RANK = {'A':13, 'K':12, 'Q':11,'J': 0 , 'T':9,'9':8, '8':7,'7':6,'6':5,'5':4,'4':3,'3':2,'2':1}
 
-
-def check_card(cards):
+def get_val(cards):
     num_cards = len(set(cards))
     if num_cards == len(cards):
-        CARDS['high'].append(cards)
+        return 'high'
     elif num_cards == 1:
-        CARDS['5 kind'].append(cards)
+        return '5 kind'
     elif num_cards == 2:
         for card in cards:
             if cards.count(card) == 3:
-                CARDS['full'].append(cards)
-                break
+                return 'full'
             if cards.count(card) == 4:
-                CARDS['4 kind'].append(cards)
-                break
+                return '4 kind'
 
     elif num_cards == 3:
         added = False
         for card in cards:
             if cards.count(card) == 3:
-                CARDS['3 kind'].append(cards)
-                added = True
-                break
-        if not added:
-            CARDS['2 pair'].append(cards)
+                return '3 kind'
+        return '2 pair'
 
     else:
-        CARDS['1 pair'].append(cards)
+        return '1 pair'
+
+def check_card(cards):
+    joker_detect = cards.count('J')
+
+    key = ''
+
+    if joker_detect == 0:
+        key = get_val(cards)
+
+    house = {card: cards.count(card) for card in cards if card != 'J'}
+
+    card_list = [card for card in house.keys()]
+    card_list.sort(key = lambda x : house[x], reverse=True)
+
+    if not card_list:
+        key = '5 kind'
+    else:
+        new_cards = cards.replace('J', card_list[0])
+        key = get_val(new_cards)
+
+    CARDS[key].append(cards)
 
 output = 0
 
@@ -54,7 +68,4 @@ with open('input.txt', 'r') as data:
 
         current_stand += len(CARDS[house])
 
-    print(RANK)
-    print(BID)
-    print(CARDS)
     print(output)
