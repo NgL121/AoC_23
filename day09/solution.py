a1cd3900fe6bdc005552dc509ef71a02acf94b12
Luong <n.luong@campus.lmu.de>
2023-12-09 13:20:35 +0100

    guess it worked...

---
+++ day09/solution.py
@@ -0,0 +1,40 @@
+with open('input.txt', 'r') as data:
+    lines = data.readlines()
+
+    output = 0
+    for line in lines:
+        history = []
+        line = [int(num) for num in line.split()]
+        history.append(line[-1])
+
+        current_history = 0
+
+        temp = line
+
+        while list(set(temp)) != [0]:
+            curr_val = temp[0]
+            if len(temp) == 1:
+                while history[-1] != 0:
+                    history.pop()
+                break
+
+            for idx, num in enumerate(temp):
+                if idx == 0:
+                    continue
+
+                point = num - curr_val
+                curr_val = num
+                temp[idx] = point
+
+                # end of a line
+                if idx == len(temp) - 1:
+                    current_history = point
+                    history.append(point)
+
+
+            temp.pop(0)
+            curr_val = temp[0]
+
+        output += sum(history)
+
+    print(output)
