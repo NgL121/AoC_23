28646d482eae956374b5bebb50d8f1df661a4105
Luong <n.luong@campus.lmu.de>
2023-12-15 10:55:30 +0100

    day 15 part 1

---
+++ day15/solution.py
@@ -0,0 +1,16 @@
+with open('input.txt', 'r') as data:
+    codes = data.readline().split(',')
+
+    print(codes)
+
+    output = 0
+    for string in codes:
+        current_point = 0
+        for char in string:
+            current_point += ord(char)
+            current_point*= 17
+            current_point %=256
+
+        output += current_point
+
+    print(output)
